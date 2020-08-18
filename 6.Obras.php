8d9e43d919a2f7577656b39143de6a6c92753bfd
NeroArt <pacto.polar88@gmail.com>
2020-08-18 01:15:04 -0500

    borrado de linea de mas

--- 6.Obras.php
+++ 6.Obras.php
@@ -1,10 +1,7 @@
 <?php
 include ("conexion.php");
-<<<<<<< HEAD
+
  $usuarios="select * from virtual_tour.obras inner join virtual_tour.autor on obras.id_aut=autor.id_autor;";
-=======
- $usuarios="select * from virtual_tour.obras INNER JOIN virtual_tour.autor ON obras.id_aut=autor.id_autor";
->>>>>>> c7bf949800b7df658a8c145ff3a4bc51753d106d
 ?>
 
 <!DOCTYPE html>
