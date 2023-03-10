0d36a9a5c5775619c0514c8a42898cb9966fd2f0
CharlX <68936309+Charliedev-11095@users.noreply.github.com>
2023-03-09 23:56:44 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
-$usuario = "root";
-$contrasena = "1M7UARTKmB296SZotGYk";
-$servidor = "containers-us-west-90.railway.app";
-$basededatos = "railway";
-$port ="7608";
-$conexion= mysqli_connect ($servidor,$usuario,$contrasena,$port) or die("No se ha podido conectar al servidor de Base de Datos");
-mysqli_set_charset($conexion,"utf8");
+<?php
+ $usuario = "root";
+ $contrasena = "";
+ $servidor = "localhost";
+ $basededatos = "virtual_tour";
+ $conexion= mysqli_connect ($servidor,$usuario,$contrasena) or die("No se ha podido conectar al servidor de Base de Datos");
+ mysqli_set_charset($conexion,"utf8");
 ?>
