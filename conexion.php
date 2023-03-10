0ac2bd61e496e4cc97e56f9db40374854cddb9ce
CharlX <68936309+Charliedev-11095@users.noreply.github.com>
2023-03-09 21:54:27 -0600

    udate conexion.php
    funcionara?

--- conexion.php
+++ conexion.php
@@ -1,14 +1,17 @@
 <?php
-$usuario = "root";
+/*$usuario = "root";
+//$contrasena = "";
+//$servidor = "localhost";
+//$basededatos = "virtual_tour";
+//$conexion= mysqli_connect ($servidor,$usuario,$contrasena) or die("No se ha podido conectar al servidor de Base de Datos");
+//mysqli_set_charset($conexion,"utf8");
+
+$usuario = "xquznttan26g3dhy1ntv";
 $contrasena = "";
-$servidor = "localhost";
+$servidor = "us-east.connect.psdb.cloud";
 $basededatos = "virtual_tour";
-
 $conexion= mysqli_connect ($servidor,$usuario,$contrasena) or die("No se ha podido conectar al servidor de Base de Datos");
-
 mysqli_set_charset($conexion,"utf8");
-
-
 
 
 ?>
