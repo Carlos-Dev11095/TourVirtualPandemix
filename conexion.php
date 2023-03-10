0be44d94246d5e169350ab033dfd38df7ce64d35
CharlX <68936309+Charliedev-11095@users.noreply.github.com>
2023-03-09 23:12:03 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,17 +1,8 @@
 <?php
-/*$usuario = "root";
-//$contrasena = "";
-//$servidor = "localhost";
-//$basededatos = "virtual_tour";
-//$conexion= mysqli_connect ($servidor,$usuario,$contrasena) or die("No se ha podido conectar al servidor de Base de Datos");
-//mysqli_set_charset($conexion,"utf8");
-
-$mysqli = mysqli_init();
-$mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-$mysqli->real_connect("us-east.connect.psdb.cloud", "3s25wv0wp72pywusjwua", "pscale_pw_bc0wKa4IHf36nutcX8MI3pP3l3rwndEKUuvzn2W37Fw", "virtual_tour");
-$conexion = $mysqli;
+$usuario = "root";
+$contrasena = "";
+$servidor = "localhost";
+$basededatos = "virtual_tour";
+$conexion= mysqli_connect ($servidor,$usuario,$contrasena) or die("No se ha podido conectar al servidor de Base de Datos");
 mysqli_set_charset($conexion,"utf8");
-
-
-
 ?>
